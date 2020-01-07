6c25f4b7da3124132ab9f5eaec6237d58b15c405
Alex Petty <pettyalex@gmail.com>
2020-01-07 13:57:27 -0600

    Tweak formatter

--- app/sunformatter.ts
+++ app/sunformatter.ts
@@ -11,12 +11,13 @@ export const formatSunString = ({
   sunrise: Date;
 }) => {
   // Return a human-readable string indicating how long until the next sunset or sunrise
+  // Return an empty string if it's a long time after sunset
   const millisecondsToSunset = sunset.valueOf() - now.valueOf();
   if (millisecondsToSunset > 0) {
     if (millisecondsToSunset < 2 * times.HOURS) {
-      return `${toMinutes(millisecondsToSunset)}M To Sunset`;
+      return `${toMinutes(millisecondsToSunset)}M Left`;
     } else {
-      return `${toHours(millisecondsToSunset)}H To Sunset`;
+      return `${toHours(millisecondsToSunset)}H Left`;
     }
   } else {
     return `${toMinutes(millisecondsToSunset)}M Since Sunset`;
